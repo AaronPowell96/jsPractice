c98d6ad44e00ff31cef06e70ce77c5182c8333ca
Aaron Powell <Apowell829@gmail.com>
2018-12-05 12:30:12 +0000

    joinArray / SplitString

---
+++ joinArraySplitString.js
@@ -0,0 +1,16 @@
+  //join array
+
+  const numbers = [1, 2, 3];
+  const joined = numbers.join(',');
+
+  console.log(joined);
+
+  //split (not for arrays, for strings)
+
+  const message = "this is my first message";
+  const parts = message.split(' ');
+  console.log(parts);
+
+  const combined = parts.join('-');
+
+  console.log(combined);
